475a3a8b50347e43d47794e5e707966bd938f183
Marco T. <marcotts@gmail.com>
2020-02-12 17:47:34 +0100

    021fork_dialog completo

--- 021fork_dialog/021fork_dialog.c
+++ 021fork_dialog/021fork_dialog.c
@@ -6,18 +6,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-//#include <libexplain/waitpid.h>
+/*
+ * questo esempio utilizza fork, execve e wait per utilizzare un applicativo esterno (dialog)
+ * per proporre all'utente un dialogo modale
+ */
 
 //extern char **environ;  /* Or define _GNU_SOURCE to get it from <unistd.h> */
 
 // è necessario dialog, su debian : sudo apt install dialog
 
-int main(int argc, char **argv) {
+
+// esercizio 1 : aggiungere come parametri alla funzione modal_dialog le stringhe del dialogo
+// esercizio 2 : riscrivere modal_dialog usando system(3) invece di fork, execve, waitpid
+
+
+int modal_dialog() {
 
 	pid_t child_pid;
-	int childExitStatus;
-	int status;
+	int wstatus;
 
+	int modal_result = -1;
 
 
 	printf("[parent] il mio PID è: %d\n", getpid());
@@ -25,25 +33,17 @@ int main(int argc, char **argv) {
 
 	if ((child_pid = fork()) == 0) {
 
-		// questo è il processo FIGLIO
+		// questo è il processo FIGLIO (child process)
 
-		printf("[child] sono il processo figlio, il mio PID è: %d\n", getpid());
+		printf("[child] sono il processo figlio (child process), il mio PID è: %d\n", getpid());
 
 		char * newargv[] = {"dialog", "--backtitle", "backtitle del dialog" ,
 		       "--title", "titolo del dialog" ,
 		       "--yesno", "\nBla bla bla...\n\nTi va bene?", "10", "30", NULL };
 
-		//char * newenviron[] = { NULL };
-
 		printf("[child] prima di execve\n");
 
-		//execve("/usr/bin/dialog", newargv, environ);
-
-
-
-		char *argv2[] = { "sleep", "5", NULL };
-		execve("/usr/bin/sleep", argv2, environ);
-
+		execve("/usr/bin/dialog", newargv, environ); // passiamo l'environment completo a dialog
 
 		perror("[child] se siamo qui, qualcosa è andato storto\n");
 
@@ -51,62 +51,49 @@ int main(int argc, char **argv) {
 
 	} else {
 
-		// questo è il processo PARENT
+		// questo è il processo PADRE (parent process)
 
-
-		printf("[parent] dopo il fork, sono il processo parent\n"
+		printf("[parent] dopo il fork, sono il processo padre\n"
 			   "[parent] il PID del processo figlio è %d\n", child_pid);
 
 		// aspettiamo il termine del processo figlio e riceviamo il risultato
-		if (wait(NULL) == -1) {
-			perror("wait error");
-		}
 
-		// https://stackoverflow.com/a/48049663/974287
+		do {
+        	pid_t ws = waitpid(child_pid, &wstatus, WUNTRACED | WCONTINUED);
+            if (ws == -1) {
+                perror("[parent] waitpid");
+                exit(EXIT_FAILURE);
+            }
 
-		pid_t ws = waitpid(child_pid, &childExitStatus, 0); //
+            if (WIFEXITED(wstatus)) {
 
-		printf("ws=%d\n", ws);
+            	modal_result = WEXITSTATUS(wstatus);
 
-		if (-1 == ws)
-		{
-			printf("was waiting for pid %d\n", child_pid);
-			perror("[parent] - failed wait.");
+                printf("[parent] child process è terminato, ha restituito: %d\n", modal_result);
+            } else if (WIFSIGNALED(wstatus)) {
+                printf("[parent] child process killed by signal %d\n", WTERMSIG(wstatus));
+            } else if (WIFSTOPPED(wstatus)) {
+                printf("[parent] child process stopped by signal %d\n", WSTOPSIG(wstatus));
+            } else if (WIFCONTINUED(wstatus)) {
+                printf("[parent] child process continued\n");
+            }
+        } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
 
-//			fprintf(stderr, "%s\n", explain_waitpid(child_pid, status, 0));
-//			exit(EXIT_FAILURE);
-
-			return -1;
-		}
-		else if (0 == ws)
-		{
-			printf("[parent] - child is still running\n");
-		}
-
-		printf("[parent] - successfully waited for child with PID %d\n", (int) ws);
-
-	    if (WIFEXITED(childExitStatus)) /* exit code in childExitStatus */
-	    {
-	    	printf("parent - got status %d", childExitStatus);
-	        status = WEXITSTATUS(childExitStatus); /* zero is normal exit */
-	        if (0 != status)
-	        {
-	        	printf("parent - picked up bad exit status\n");
-	            return status;
-	        }
-	        return 0;
-	    }
-	    else
-	    {
-	    	printf("parent - bad exit route\n");
-	        return -1;
-	    }
-
-
+        return modal_result;
 
 	}
 
+}
 
+
+
+int main(int argc, char **argv) {
+
+	int scelta_utente = modal_dialog();
+
+	system("/usr/bin/clear"); // puliamo il terminale
+
+	printf("scelta_utente = %d\n", scelta_utente);
 
 
 	return 0;
