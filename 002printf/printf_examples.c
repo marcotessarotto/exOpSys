ea5f7be5e07b478c233eefc930b841c759ff31c1
Marco T. <marcotts@gmail.com>
2020-02-21 12:19:40 +0100

    wip

--- 002printf/printf_examples.c
+++ 002printf/printf_examples.c
@@ -5,6 +5,65 @@
 
 int main(int argc, char **argv) {
 
+	char ch;
+	int a, b;
+	unsigned int ua, ub;
+	long la, lb;
+
+	float c;
+	double d;
+
+	char * str = "hello world!";
+	char * numeri = "0123456789";
+
+	ch = 'Z';
+	a = 10;
+	b = -20;
+
+	ua = 0xFFFFFFFF;
+	ub = 0;
+
+	la = 1L << 32; // provare: 1 << 32
+
+	c = 3.1492;
+
+	d = 0.123456789;
+
+	printf("scrivo un char: %c\n", ch);
+
+	printf("scrivo int: a=%d b=%d\n", a, b);
+
+	printf("scrivo unsigned int: ua=%u ub=%x \n", ua, ub);
+
+	printf("scrivo long: la=%ld\n", la);
+
+	printf("scrivo float: %f\n", c);
+
+	printf("scrivo float: %1.3f\n", c);
+
+	printf("scrivo double: %e\n", d);
+	printf("scrivo double: %1.12f\n", d);
+
+	printf("scrivo stringa di caratteri: %s\n", str);
+	printf("scrivo stringa di caratteri: %s\n", numeri);
+
+
+	printf("\n");
+
+	printf("sizeof(char)=%ld\n", sizeof(char));
+
+	printf("sizeof(short int)=%ld\n", sizeof(short int));
+	printf("sizeof(int)=%ld\n", sizeof(int));
+
+	printf("sizeof(long)=%ld\n", sizeof(long));
+	printf("sizeof(long long)=%ld\n", sizeof(long long));
+
+
+	printf("sizeof(float)=%ld\n", sizeof(float));
+	printf("sizeof(double)=%ld\n", sizeof(double));
+	printf("sizeof(long double)=%ld\n", sizeof(long double));
+
+	printf("sizeof(char *)=%ld\n", sizeof(char *));
 
 
 	/*
