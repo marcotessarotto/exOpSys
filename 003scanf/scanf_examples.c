ae5ed48e9cdfdcbbf8484f7083c3aebfedfc3375
Marco T. <marcotts@gmail.com>
2020-02-21 14:06:30 +0100

    wip

--- 003scanf/scanf_examples.c
+++ 003scanf/scanf_examples.c
@@ -3,8 +3,34 @@
 
 int main(int argc, char **argv) {
 
+	int a,b;
+	int sum;
+	int res;
 
-	// in progress...
+	res = scanf("%d %d", &a, &b);
+
+	printf("scanf ha restituito: %d\n", res);
+
+	sum = a + b;
+
+	printf("sum = %d\n", sum);
+
+	/*
+	 * esercizio:
+	 *
+	 * chiedere all'utente tre input:
+	 * l'operazione da fare (un char tra: + - /) e due numeri
+	 * eseguire l'operazione richiesta e scrivere il risultato
+	 *
+	 * esempio:
+	 *
+	 * input:
+	 * + 10 20
+	 *
+	 * risultato:
+	 * 30
+	 *
+	 */
 
 	return 0;
 }
