96764f5cf071ea2b6e484df468de7d4e8c78e35d
Marco T. <marcotts@gmail.com>
2020-02-25 15:47:52 +0100

    UTF-8 example

--- 002printf/printf_examples.c
+++ 002printf/printf_examples.c
@@ -77,6 +77,10 @@ int main(int argc, char **argv) {
 
 	printf("len(utf_8_str) = %ld\n", strlen(utf_8_str));
 
+	for (int i = 0; i < strlen(utf_8_str) + 1; i++) {
+		printf("utf_8_str[%d] = %d\n", i, (unsigned char)(utf_8_str[i]));
+	}
+
 
 	/*
 
