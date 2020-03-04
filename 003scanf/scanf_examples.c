ab9d6c0b5acf48ebeeb25fb87cc436c37e8736c5
Marco T. <marcotts@gmail.com>
2020-03-04 18:36:53 +0100

    wip

--- 003scanf/scanf_examples.c
+++ 003scanf/scanf_examples.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 
 
+void altri_esempi_scanf() {
+
+	char ch;
+	int a, b;
+	unsigned int ua, ub;
+	long la, lb;
+
+	float c;
+	double d;
+
+	printf("scrivi un char: ");
+	scanf("%c", &ch);
+	printf("ho letto il seguente char: %c\n", ch);
+
+	printf("scrivi un int: ");
+	scanf("%d", &a);
+	printf("ho letto il seguente int: %d\n", a);
+
+
+}
+
+
 int main(int argc, char **argv) {
 
 	int a,b;
 	int sum;
 	int res;
 
+	//altri_esempi_scanf();
+
+	//return 0;
+
+	printf("scrivi due numeri interi: ");
+
 	res = scanf("%d %d", &a, &b);
 
-	printf("scanf ha restituito: %d\n", res);
+	printf("scanf ha restituito: %d (numero di valori letti)\n", res);
+
+	if (res == EOF) {
+		printf("EOF! bye!");
+		return 1;
+	}
 
 	sum = a + b;
 
