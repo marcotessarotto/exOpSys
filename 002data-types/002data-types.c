f046fbd592f64bfe41346dacc57b8aadecf404e6
Marco T. <marcotts@gmail.com>
2020-03-11 17:41:12 +0100

    002data-types project

--- 002data-types/002data-types.c
+++ 002data-types/002data-types.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <limits.h>
 
+#include <float.h>
+
 int main(int argc, char **argv) {
 
 	char ch;
@@ -36,13 +38,43 @@ int main(int argc, char **argv) {
 
 	// esercizio: mostrare le stesse informazioni per unsigned int
 
-
 	// long
 	printf("\n***long***\n");
 	printf("valore minimo di long: %ld\n", LONG_MIN);
 	printf("valore massimo di long: %ld\n", LONG_MAX);
 	printf("sizeof(long) = %ld byte\n", sizeof(long));
 
+	// esercizio: mostrare le stesse informazioni per unsigned long
+
+
+	// tipi dati a virgola mobile
+
+	// https://www.tutorialspoint.com/c_standard_library/float_h.htm
+
+	printf("\n***float***\n");
+	printf("The maximum value of float = %.10e\n", FLT_MAX);
+	printf("The minimum value of float = %.10e\n", FLT_MIN);
+	printf("sizeof(float) = %ld byte\n", sizeof(float));
+
+	// provare questo:
+	//printf("The maximum value of float = %f\n", FLT_MAX);
+	//printf("The minimum value of float = %f\n", FLT_MIN);
+
+	printf("\n***double***\n");
+	printf("The maximum value of double = %.10e\n", DBL_MAX);
+	printf("The minimum value of double = %.10e\n", DBL_MIN);
+	printf("sizeof(double) = %ld byte\n", sizeof(double));
+
+	//printf("The maximum value of double = %f\n", DBL_MAX);
+	//printf("The minimum value of double = %f\n", DBL_MIN);
+
+	printf("\n***long double***\n");
+	printf("The maximum value of long double = %.10Le\n", LDBL_MAX);
+	printf("The minimum value of long double = %.10Le\n", LDBL_MIN);
+	printf("sizeof(long double) = %ld byte\n", sizeof(long double));
+
+	//printf("The maximum value of long double = %Lf\n", LDBL_MAX);
+	//printf("The minimum value of long double = %Lf\n", LDBL_MIN);
 
 	return 0;
 }
