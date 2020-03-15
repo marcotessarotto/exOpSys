96b2b9b8f99f28089779bb6540ce4b5366fb0b01
marco <marcotts@gmail.com>
2020-03-15 15:34:53 +0100

    add comments

--- 098getfilestatus/getfilestatus.c
+++ 098getfilestatus/getfilestatus.c
@@ -26,6 +26,10 @@ main(int argc, char *argv[])
    printf("ID of containing device:  [%lx,%lx]\n",
 		(long) major(sb.st_dev), (long) minor(sb.st_dev));
 
+   printf("I-node number:            %ld\n", (long) sb.st_ino);
+   // device ID + I-node: uniquely identifies a file across all file systems
+
+
    printf("File type:                ");
 
    switch (sb.st_mode & S_IFMT) {
@@ -39,10 +43,9 @@ main(int argc, char *argv[])
 	   default:       printf("unknown?\n");                break;
    }
 
-   printf("I-node number:            %ld\n", (long) sb.st_ino);
 
    printf("Mode:                     %lo (octal)\n",
-		   (unsigned long) sb.st_mode);
+		   (unsigned long) sb.st_mode & ~S_IFMT);
 
    printf("Link count:               %ld\n", (long) sb.st_nlink);
    printf("Ownership:                UID=%ld   GID=%ld\n",
@@ -54,6 +57,13 @@ main(int argc, char *argv[])
 		   (long long) sb.st_size);
    printf("Blocks allocated:         %lld\n",
 		   (long long) sb.st_blocks);
+
+   // Kerrisk pag. 286
+
+   // read(): access
+   // write(): modification, change
+   // open() create: access, modification, change
+   // open() truncate: modification, change
 
    printf("Last status change:       %s", ctime(&sb.st_ctime));
    printf("Last file access:         %s", ctime(&sb.st_atime));
