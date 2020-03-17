b5a654a97cd3cd8682fb46a1beb1d536283fd504
marco <marcotts@gmail.com>
2020-03-17 17:54:26 +0100

    wip

--- 004arrays/arrays-example.c
+++ 004arrays/arrays-example.c
@@ -11,7 +11,7 @@ void read_static_array() {
 	// ogni elemento è inizializzato a 0
 	int numbers[10] = { 0 };
 
-	//printf("dimensione totale dell'array numbers: %ld\n", sizeof(numbers));
+	printf("dimensione totale dell'array numbers: %ld\n", sizeof(numbers));
 
 	printf("scrivi 10 numeri interi: ");
 
@@ -23,7 +23,7 @@ void read_static_array() {
 
 	printf("\n");
 
-	for (int i=0; i < 10; i++) {
+	for (unsigned int i=0; i < 10; i++) {
 		printf("numbers[%u]=%d\n", i, numbers[i]);
 	}
 
@@ -34,8 +34,17 @@ void read_dynamic_array(unsigned int array_size) {
 	int * numbers;
 
 	numbers = calloc(array_size, sizeof(int));
+	// prova ad allocare una zona di memoria di dimensione totale (bytes)
+	// array_size * sizeof(int)
+	// e la inizializza a zero
+
+	// in Java:
+	// int [] numbers = new int[array_size];
+
+
 
 	// ESERCIZIO 1: scrivi la dimensione in bytes dell'array dinamico
+	//
 
 	// ESERCIZIO 2: completare:
 
@@ -52,22 +61,26 @@ void read_dynamic_array(unsigned int array_size) {
 int main() {
 
 	int first_array[3] = { 0x123, 0x1234, 0x12345 };
-	// ogni cella dell'array ha dimensione (in bytes) ...
+	// ogni cella dell'array ha dimensione (in bytes) : 4 bytes
 	// gli indici validi di first_array sono: 0, 1, 2
-	// dimensione totale dell'array in bytes: ....
+	// dimensione totale dell'array in bytes: 12 bytes
+
+	int var1 = 0x00000123;
 
 	/*
+	 * 00 00 01 23
+	 *
 	   ESERCIZIO: completare la rappresentazione in memoria degli array
 
                         Big endian (Intel, AMD)
        +----+----+----+----+----+----+----+----+----+----+----+----+
-       |0x00|    |    |    |    |    |    |    |    |    |    |    |
+       |0x00|0x00|0x01|0x23|0x00|0x00|0x12|0x34|0x00|0x01|0x23|0x45|
        +----+----+----+----+----+----+----+----+----+----+----+----+
  byte     0    1    2    3   4     5    6    7    8    9   10   11
  address
 						Little endian (ARM)
        +----+----+----+----+----+----+----+----+----+----+----+----+
-       |    |    |    |    |    |    |    |    |    |    |    |    |
+       |0x23|0x01|0x00|0x00|0x34|0x12|0x00|0x00|0x45|0x23|0x01|0x00|
        +----+----+----+----+----+----+----+----+----+----+----+----+
  byte     0    1    2    3   4     5    6    7    8    9   10   11
  address
@@ -75,33 +88,35 @@ int main() {
 
 	 */
 
-	int second_array[5] = {2, 1, 0};
-	// ogni cella dell'array ha dimensione (in bytes) ...
-	// gli indici validi di second_array sono: 0, 1, ....
-	// dimensione totale dell'array in bytes: ....
+	int second_array[5] = {2, 1, 0}; // equivalente a {2,1,0,0,0}
+	// ogni cella dell'array ha dimensione (in bytes) 4 bytes
+	// gli indici validi di second_array sono: 0, 1, 2, 3, 4
+	// dimensione totale dell'array in bytes: 4 * 5 = 20 bytes
 
-	short int third_array[10];
-	// ogni cella dell'array ha dimensione (in bytes) ...
-	// gli indici validi di second_array sono: ....
-	// dimensione totale dell'array in bytes: ....
+	second_array[4] = 123123; // le celle dell'array sono pronte per l'uso
+
+	short int third_array[10]; // le celle dell'array hanno valori casuali
+	// ogni cella dell'array ha dimensione (in bytes) : 2 bytes
+	// gli indici validi di third_array sono: [0, 9]
+	// dimensione totale dell'array in bytes: 20 bytes
 
 	char fourth_array[1024];
-	// ogni cella dell'array ha dimensione (in bytes) ...
-	// gli indici validi di second_array sono: ....
-	// dimensione totale dell'array in bytes: ....
+	// ogni cella dell'array ha dimensione (in bytes) 1 byte
+	// gli indici validi di fourth_array sono: [0, 1023]
+	// dimensione totale dell'array in bytes: 1024 bytes
 
 
 	first_array[0] = -1;
 
 	// non viene fatto un controllo sugli indici quando accedo all'array,
-	// dipende dal programma
+	// dipende dal programmatore
 
 	// provare!!! se messaggio di errore riporta un signal,
 	// andare a vedere: man 7 signal
-	//first_array[100000] = 0XDEADBEEF; // errore! 1000 è un indice NON valido
+	// first_array[100000] = 0XDEADBEEF; // errore! 100000 è un indice NON valido
 
 	int z = first_array[2] + first_array[0];
-	// quanto vale z? ....
+	// quanto vale z? 0x12344
 
 	if (1) {
 
