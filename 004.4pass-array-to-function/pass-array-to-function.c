18b9e7467bf52ca0f73b4a5fc2d00665a37c7c3a
Marco T. <marcotts@gmail.com>
2020-03-18 11:23:19 +0100

    add project 004.4pass-array-to-function

---
+++ 004.4pass-array-to-function/pass-array-to-function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char * argv[]) {
+
+	int * array;
+
+	int dimension = 1024;
+
+	// NOTA: malloc, calloc e free richiedono:
+	// #include <stdlib.h>
+
+	array = calloc(dimension, sizeof(int));
+
+
+
+}
