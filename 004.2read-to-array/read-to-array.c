27e76c0d2efcf61cbe1cd30330de614cb549dab1
marco <marcotts@gmail.com>
2020-03-20 17:59:10 +0100

    in progress

--- 004.2read-to-array/read-to-array.c
+++ 004.2read-to-array/read-to-array.c
@@ -10,10 +10,17 @@ static void sig_handler(int sig) {
 	printf("ho ricevuto il segnale %d\n", sig);
 }
 
+//#define DESCRITTORE_ZERO 0
+//const int FD0 = DESCRITTORE_ZERO;
+
 int main(int argc, char * argv[]) {
 
 	// il file descriptor 0 è stato predisposto dal kernel per ...
 	// quali altri file descriptor sono pronti per l'uso?
+
+	// provate questo (da shell):
+	// /home/utente/git/exOpSys/004.2read-to-array
+	// Debug/004.2read-to-array < /home/utente/prova.txt
 
 	// come trovo il process id di questo programma?
 	// da shell: https://www.youtube.com/watch?v=JxSc5mQltII
@@ -24,7 +31,7 @@ int main(int argc, char * argv[]) {
 	printf("il mio process id è %u\n", pid);
 
 
-	// provare a mandare il segnale -2 al programma
+	// provare a mandare il segnale 2 al programma
 	if (signal(SIGINT, sig_handler) == SIG_ERR) {
 		perror("signal");
 	}
@@ -42,7 +49,14 @@ int main(int argc, char * argv[]) {
 	// system call read
 	// chiediamo al kernel di leggere dal file descriptor 0 (stdin)
 	// fino a (size * sizeof(char)) bytes
-	res = read(STDIN_FILENO, data, size * sizeof(char));
+
+//	while ((res = read(.....)) > 0) {
+//
+//	}
+
+	res = read(STDIN_FILENO , data, size * sizeof(char));
+	// esattamente equivalente a:
+	// res = read(0 , data, size * sizeof(char));
 
 	if (res == -1) {
 		perror("read error/signal");
