2440f25705eadd5d34443b31c78d9aa74e4ac0ff
Marco T. <marcotts@gmail.com>
2020-03-24 15:31:33 +0100

    add 005.1getchar

---
+++ 005.1getchar/getchar-example.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+	int c;
+	unsigned long counter = 0;
+
+	// in Unix (e Linux) ogni processo, quando viene avviato, ha tre file descriptors (descrittori di file) pronti per l'uso:
+
+	// standard input (0, stdin) da cui leggere
+	// standard output (1, stdout) su cui scrivere
+	// standard error (2, stderr) su cui scrivere messaggi di errore o diagnostici
+
+	// tipicamente sono "attaccati" al terminale da cui viene avviato il processo
+	// quindi: input da tastiera, output (1 e 2) su schermo
+
+
+	if (1) {
+
+		c = getchar();
+
+		while (c != EOF) {
+
+			counter++;
+
+			// utilizza c
+			// ....
+
+			c = getchar();
+		}
+
+	} else {
+
+		// equivalente a:
+
+		while ((c = getchar()) != EOF) {
+
+			counter++;
+
+			// utilizza c
+			// ....
+
+		}
+
+	}
+
+	printf("\nho letto %lu caratteri\n", counter);
+
+	return 0;
+}
