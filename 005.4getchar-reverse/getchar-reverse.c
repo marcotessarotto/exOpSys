2c258f6bb15d16e63ff4440ef942beb241c19b42
Marco T. <marcotts@gmail.com>
2020-03-24 16:03:49 +0100

    add 005.4getchar-reverse

---
+++ 005.4getchar-reverse/getchar-reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+	// ESERCIZIO 2:
+
+	// scrivere il seguente programma:
+
+	// legge e salva tutti i dati provenienti da stdin fino a EOF
+
+	// poi scrive su stdout i dati accumulati ma all'incontrario (dall'ultimo al primo)
+
+	// suggerimenti: .....
+
+	return 0;
+}
