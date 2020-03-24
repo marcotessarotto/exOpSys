750cfced9248430c5ebd04fdcb5d53b4e870a7ef
marco <marcotts@gmail.com>
2020-03-24 18:12:15 +0100

    comments

--- 004.6array-comparison/array-comparison.c
+++ 004.6array-comparison/array-comparison.c
@@ -15,8 +15,8 @@ int my_memcmp(const void* s1, const void* s2,size_t n)
         if( *p1 != *p2 ) {
             return *p1 - *p2;
         } else {
-            p1++;
-            p2++;
+            p1++; // incremento di puntatore di tipo unsigned char * (+1 byte)
+            p2++; // idem
         }
     return 0;
 }
