77dbbb6b37d823c16d6999c7f87115f8473cdaf9
Marco T. <marcotts@gmail.com>
2020-03-24 16:04:26 +0100

    add 005.3countchars

---
+++ 005.3countchars/countchars.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+
+	// ESERCIZIO 1:
+	// leggi da stdin fino a EOF e conta i caratteri: '\n', '\t' e ' '
+	// dopo EOF, scrive su stdout il numero di occorrenze di ciascun carattere
+
+
+	return 0;
+}
