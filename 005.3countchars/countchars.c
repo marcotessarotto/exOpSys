76ffc600f2e2db15a1c537c2b380cebe46ea45dd
Marco T. <marcotts@gmail.com>
2020-04-03 13:58:51 +0200

    solution

--- 005.3countchars/countchars.c
+++ 005.3countchars/countchars.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
 	// leggi da stdin fino a EOF e conta i caratteri: '\n', '\t' e ' ' (spazio)
 	// dopo EOF, scrive su stdout il numero di occorrenze di ciascuno dei tre caratteri sopra
 
-	char ch;
+	int ch;
 
  	int char_occurrence[256] = { 0 };
 
