13d9a3ad347327995ab844b2b033c907359bac21
marco <marcotts@gmail.com>
2020-04-03 20:11:58 +0200

    use stretchy_buffer.h in 005.4getchar-reverse

--- 005.4getchar-reverse/getchar-reverse.c
+++ 005.4getchar-reverse/getchar-reverse.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "stretchy_buffer.h"
+
+void alternative_solution() {
+
+
+	//       SomeStruct *arr = NULL;
+	//       while (something)
+	//       {
+	//          SomeStruct new_one;
+	//          new_one.whatever = whatever;
+	//          new_one.whatup   = whatup;
+	//          new_one.foobar   = barfoo;
+	//          sb_push(arr, new_one);
+	//       }
+
+	int ch;
+	char *arr = NULL;
+	unsigned int counter = 0;
+
+	while ((ch = getchar()) != EOF) {
+
+		sb_push(arr, ch);
+
+		counter++;
+	}
+
+
+	printf("caratteri letti=%u\n", counter);
+
+	// poi scrive su stdout i dati accumulati ma all'incontrario (dall'ultimo al primo)
+
+	printf("ecco i contenuti all'incontrario:\n");
+
+	for (int i = counter-1; i >= 0; i--) {
+		//printf("i=%d\n", i);
+		putchar(arr[i]);
+	}
+
+	sb_free(arr);
+
+
+}
+
 
 int main(int argc, char *argv[]) {
+
+	if (1) {
+
+		// utilizza:
+		// stretchy_buffer.h - v1.04 - public domain - nothings.org/stb
+		// a vector<>-like dynamic array for C
+
+		// https://github.com/nothings/stb/blob/master/stretchy_buffer.h
+
+		alternative_solution();
+
+		return 0;
+	}
 
 	// ESERCIZIO 2:
 
