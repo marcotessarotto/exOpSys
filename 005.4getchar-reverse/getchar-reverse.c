d16f43e55cd697c0a7abb817aa10cc62654bb00e
Marco T. <marcotts@gmail.com>
2020-04-03 14:08:40 +0200

    solution

--- 005.4getchar-reverse/getchar-reverse.c
+++ 005.4getchar-reverse/getchar-reverse.c
@@ -10,9 +10,45 @@ int main(int argc, char *argv[]) {
 
 	// legge e salva (in memoria) tutti i dati provenienti da stdin fino a EOF
 
+	int ch;
+	char * array;
+	unsigned int counter = 0;
+
+	array = malloc(sizeof(char));
+
+	if (array == NULL) {
+		perror("malloc()");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getchar()) != EOF) {
+
+		array[counter] = ch;
+
+		counter++;
+
+		//printf("counter=%d\n", counter);
+		array = realloc(array, (counter+1) * sizeof(char));
+
+		if (array == NULL) {
+			perror("realloc()");
+			exit(EXIT_FAILURE);
+		}
+
+	}
+
+	printf("caratteri letti=%u\n", counter);
+
 	// poi scrive su stdout i dati accumulati ma all'incontrario (dall'ultimo al primo)
 
-	// suggerimenti: utilizzare getchar(), calloc(), realloc()
+	printf("ecco i contenuti all'incontrario:\n");
+
+	for (int i = counter-1; i >= 0; i--) {
+		//printf("i=%d\n", i);
+		putchar(array[i]);
+	}
+
+	free(array); // non necessario
 
 	return 0;
 }
