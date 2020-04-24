6bd8188175c5d4cc380808cbce2befebac65557b
marco <marcotts@gmail.com>
2020-04-24 17:46:46 +0200

    update

--- 018simple_pipes/simple_pipe.c
+++ 018simple_pipes/simple_pipe.c
@@ -52,15 +52,14 @@ int main(int argc, char * argv[]) {
 
 #ifdef USE_EXEC
 
-			if (pfd[0] != STDIN_FILENO) {
 
-				if (dup2(pfd[0], STDIN_FILENO) == -1) { // chiude fd 0; riapre fd 0 e lo rende identico a fd 3 (cioè fd 3 e fd 0 puntano allo stesso "file", in questo caso l'estremità di una pipe)
-					perror("problema con dup2");
-					exit(EXIT_FAILURE);
-				}
-
-				close(pfd[0]); // non ci serve più, perchè a wc passeremo fd 0 (che ora è l'estremità read della pipe)
+			if (dup2(pfd[0], STDIN_FILENO) == -1) { // chiude fd 0; riapre fd 0 e lo rende identico a fd 3 (cioè fd 3 e fd 0 puntano allo stesso "file", in questo caso l'estremità di una pipe)
+				perror("problema con dup2");
+				exit(EXIT_FAILURE);
 			}
+
+			close(pfd[0]); // non ci serve più, perchè a wc passeremo fd 0 (che ora è l'estremità read della pipe)
+
 
 			// al programma wc passeremo i seguenti file descriptor aperti:
 			// 0: l'estremità di lettura della pipe (al posto di stdin)
@@ -108,15 +107,13 @@ int main(int argc, char * argv[]) {
 
 #ifdef USE_EXEC
 
-			if (pfd[1] != STDOUT_FILENO) {
-
-				if (dup2(pfd[1], STDOUT_FILENO) == -1) { // chiude fd 1; riapre fd 1 e lo rende identico a fd 4 (cioè fd 1 e fd 4 puntano allo stesso file...)
-					perror("problema con dup2");
-					exit(EXIT_FAILURE);
-				}
-
-				close(pfd[1]);
+			if (dup2(pfd[1], STDOUT_FILENO) == -1) { // chiude fd 1; riapre fd 1 e lo rende identico a fd 4 (cioè fd 1 e fd 4 puntano allo stesso file...)
+				perror("problema con dup2");
+				exit(EXIT_FAILURE);
 			}
+
+			close(pfd[1]); // pfd[1] == 4
+
 
 			execlp("ls", "ls", "-l", "/home/utente", (char *) NULL);
 
