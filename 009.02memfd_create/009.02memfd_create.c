f68d074d82aa1c5cf505b3103ea5a39135819c50
Marco T. <marcotts@gmail.com>
2020-06-15 18:55:12 +0200

    in progress

--- 009.02memfd_create/009.02memfd_create.c
+++ 009.02memfd_create/009.02memfd_create.c
@@ -8,11 +8,17 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <ctype.h>
+
+#include <errno.h>
 
 /*
  *
  * il processo padre crea un file temporaneo con la system call memfd_create.
- * prova di F_SEAL_SHRINK, passaggio del file descriptor tra processi via UNIX domain socket
+ * prova di utilizzo di F_SEAL_SHRINK.
+ * passaggio del file descriptor tra processi tramite UNIX domain socket
  *
  *
  *
@@ -40,15 +46,21 @@
 
 #define FILE_SIZE (100*1024*1024)
 
+#define BUF_SIZE 256
+
 const char * tmp_file_name = "tmpfile";
 
 __off_t get_fd_size(int fd);
 
 
+#define SERVER_SOCKET_PATH "/tmp/unix_domain_socket"
+
+void parent_process();
 
 int main(int argc, char * argv[]) {
 
 	int fd;
+	int flags;
 
 	// crea file temporaneo, la dimensione iniziale del file è 0
 	if ((fd = memfd_create(tmp_file_name, MFD_ALLOW_SEALING | MFD_CLOEXEC)) == -1) {
@@ -56,6 +68,12 @@ int main(int argc, char * argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
+/*
+		By default, the new file descriptor is set to remain open across an execve(2)
+		(i.e., the FD_CLOEXEC file descriptor  flag  described  in  fcntl(2)  is  initially  disabled);
+		the O_CLOEXEC flag, described below, can be used to change this default.
+		The file offset is set to the beginning of the file (see lseek(2)).
+*/
 	// F_SEAL_SHRINK
     //If this seal is set, the file in question cannot be reduced in
     //size.  This affects open(2) with the O_TRUNC flag as well as
@@ -66,14 +84,14 @@ int main(int argc, char * argv[]) {
 		perror("fcntl");
 	}
 
-	if (fcntl(fd, F_SETFD, FD_CLOEXEC) == -1) {
-		perror("fcntl");
-	}
-
+	// imposta la dimensione del file
 	if (ftruncate(fd, FILE_SIZE) == -1) {
 		perror("ftruncate");
 		exit(EXIT_FAILURE);
 	}
+
+	// TODO: create memory map
+
 
 	__off_t file_size;
 
@@ -84,36 +102,6 @@ int main(int argc, char * argv[]) {
 	int pid;
 	pid = getpid();
 	printf("[parent] pid = %d\n", pid);
-
-	if (fork() == 0) {
-
-		char cmd0[256] = { 0 };
-
-		file_size = get_fd_size(fd);
-		printf("[child] file_size = %ld bytes\n", file_size);
-
-
-
-		sprintf(cmd0, "/proc/%d/fd", pid);
-
-		printf("[child] ls -l %s\n", cmd0);
-
-
-		pid = getpid();
-		printf("[child] pid = %d\n", pid);
-
-		//getchar();
-
-
-		execlp("ls", "ls", "-l", cmd0, NULL);
-
-		exit(EXIT_FAILURE);
-
-	}
-
-	// aspettiamo la conclusione del processo figlio (se è stato avviato correttamente)
-	wait(NULL);
-
 
 /*
  * A second process obtains a file descriptor for the tmpfs(5) file
@@ -140,11 +128,68 @@ int main(int argc, char * argv[]) {
  */
 
 
-	// TODO:
-	// crea un processo figlio;
-	// comunica col processo figlio attraverso UNIX domain socket
+	// crea un UNIX domain socket
+
+	struct sockaddr_un svaddr, claddr;
+	int sfd;
+
+	if (unlink(SERVER_SOCKET_PATH) == -1 && errno != ENOENT) {
+		perror("unlink");
+		exit(EXIT_FAILURE);
+	}
+
+    if (strlen(SERVER_SOCKET_PATH) > sizeof(svaddr.sun_path) - 1) {
+    	printf("Server socket path too long: %s", SERVER_SOCKET_PATH);
+    	exit(EXIT_FAILURE);
+    }
+
+    sfd = socket(AF_UNIX, SOCK_DGRAM, 0); // Create server socket
+    if (sfd == -1) {
+    	perror("socket");
+    	exit(EXIT_FAILURE);
+    }
 
 
+    memset(&svaddr, 0, sizeof(struct sockaddr_un));
+    svaddr.sun_family = AF_UNIX;
+    strncpy(svaddr.sun_path, SERVER_SOCKET_PATH, sizeof(svaddr.sun_path) - 1);
+
+    if (bind(sfd, (struct sockaddr *) &svaddr, sizeof(struct sockaddr_un)) == -1) {
+    	perror("bind");
+    	exit(EXIT_FAILURE);
+    }
+
+    // il server socket è pronto
+
+    // crea un processo figlio
+    if (fork() == 0) {
+    	// comunica col processo figlio attraverso UNIX domain socket
+    	parent_process();
+    }
+
+
+    ssize_t numBytes;
+    socklen_t len;
+    char buf[BUF_SIZE];
+
+    for (;;) {
+
+    	// aspettiamo la richiesta di inviare il file descriptor dal client
+
+        len = sizeof(struct sockaddr_un);
+        numBytes = recvfrom(sfd, buf, BUF_SIZE, 0,
+                            (struct sockaddr *) &claddr, &len);
+        if (numBytes == -1) {
+        	perror("recvfrom");
+        	exit(EXIT_FAILURE);
+        }
+
+        printf("[parent ] received %ld bytes from %s\n", (long) numBytes, claddr.sun_path);
+
+
+        // TODO:
+        // rispondi ...
+    }
 
 
 	if (close(fd) == -1) {
@@ -158,6 +203,11 @@ int main(int argc, char * argv[]) {
 	return 0;
 }
 
+void parent_process() {
+
+
+	exit(EXIT_SUCCESS);
+}
 
 // restituisce la dimensione del file, -1 in caso di errore
 __off_t get_fd_size(int fd) {
