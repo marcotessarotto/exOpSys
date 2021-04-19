c2a1d88f88e4c485cc0455e2a91824a50c39be55
Marco T. <marcotts@gmail.com>
2021-04-19 12:49:51 +0200

    update

--- 016printenvvars/printenvvars.c
+++ 016printenvvars/printenvvars.c
@@ -5,6 +5,10 @@ extern char **environ;  /* Or define _GNU_SOURCE to get it from <unistd.h> */
 
 int main(int argc, char *argv[])
 {
+	printf("program: %s\n", argv[0]);
+
+	printf("environment:\n\n");
+
     char **ep;
 
     for (ep = environ; *ep != NULL; ep++)
