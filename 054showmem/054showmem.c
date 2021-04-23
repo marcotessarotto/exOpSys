17bc75a8caa12fa71f4a5686735595e4935271f1
Marco T. <marcotts@gmail.com>
2021-04-23 11:53:56 +0200

    update

--- 054showmem/054showmem.c
+++ 054showmem/054showmem.c
@@ -10,17 +10,26 @@
 
 extern char **environ;  /* Or define _GNU_SOURCE to get it from <unistd.h> */
 
+// questa variabile si trova nel segmento dei dati inizializzati
 char prova[] = "012345678";
+
+extern char etext, edata, end;
+/*
+      The addresses of these symbols indicate the end of various program segments:
+
+       etext  This is the first address past the end of the text segment (the program code).
+       edata  This is the first address past the end of the initialized data segment.
+       end    This is the first address past the end of the uninitialized data segment (also known as the BSS segment).
+*/
 
 int main() {
 
 	// il programma mostra i contenuti di /proc/<pid>/maps per il processo corrente
 
-	// man 5 proc
-
 	char * program_filename = "/usr/bin/cat";
-	char param[256];
-	//char * proc_self = "/proc/self/maps";
+
+	// static: questa variabile si trova nel segmento dei dati non inzializzati
+	static char param[256];
 
 	int pid;
 
@@ -30,7 +39,6 @@ int main() {
 
 	printf("il comando che il processo figlio eseguirà é:\n%s %s\n\n", program_filename, param);
 	//printf("le informazioni del processo corrente si trovano anche in /proc/self/\n\n");
-	// "/proc/self/maps"
 
 	// allochiamo dei dati sulla heap memory, così lo vedremo in maps
 	//char * mem = malloc(1024);
@@ -66,6 +74,19 @@ int main() {
 	printf("indirizzo di main(): %p\n", main);
 
 	printf("indirizzo della variabile 'prova': %p\n", &prova);
+
+	printf("indirizzo della variabile 'param': %p\n", &param);
+
+	printf("indirizzo della variabile 'program_filename' [stack]: %p\n", &program_filename);
+
+
+	printf("indirizzo di etext: %p\n", &etext);
+	printf("indirizzo di edata: %p\n", &edata);
+	printf("indirizzo di end: %p\n", &end);
+
+	printf("\netext  This is the first address past the end of the text segment (the program code).\n"
+       "edata  This is the first address past the end of the initialized data segment.\n"
+       "end    This is the first address past the end of the uninitialized data segment (also known as the BSS segment).\n");
 
 
 /*
@@ -107,42 +128,40 @@ aspetti interessanti:
 - [stack]
 
 address                   perms offset  dev   inode                       pathname
-558fa68de000-558fa68e0000 r--p 00000000 00:19 5840119                    /usr/bin/cat
-558fa68e0000-558fa68e5000 r-xp 00002000 00:19 5840119                    /usr/bin/cat
-558fa68e5000-558fa68e8000 r--p 00007000 00:19 5840119                    /usr/bin/cat
-558fa68e8000-558fa68e9000 r--p 00009000 00:19 5840119                    /usr/bin/cat
-558fa68e9000-558fa68ea000 rw-p 0000a000 00:19 5840119                    /usr/bin/cat
-558fa730a000-558fa732b000 rw-p 00000000 00:00 0                          [heap]
-7fc375b87000-7fc375ba9000 rw-p 00000000 00:00 0
-7fc375ba9000-7fc375bfe000 r--p 00000000 00:19 7660450                    /usr/lib/locale/aa_DJ.utf8/LC_CTYPE
-7fc375bfe000-7fc375e76000 r--p 00000000 00:19 7660449                    /usr/lib/locale/aa_DJ.utf8/LC_COLLATE
-7fc375e76000-7fc375e9b000 r--p 00000000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc375e9b000-7fc375fe6000 r-xp 00025000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc375fe6000-7fc376030000 r--p 00170000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc376030000-7fc376031000 ---p 001ba000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc376031000-7fc376034000 r--p 001ba000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc376034000-7fc376037000 rw-p 001bd000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
-7fc376037000-7fc37603d000 rw-p 00000000 00:00 0
-7fc376049000-7fc37604a000 r--p 00000000 00:19 7660472                    /usr/lib/locale/aa_ET/LC_NUMERIC
-7fc37604a000-7fc37604b000 r--p 00000000 00:19 7661328                    /usr/lib/locale/en_US.utf8/LC_TIME
-7fc37604b000-7fc37604c000 r--p 00000000 00:19 7660947                    /usr/lib/locale/chr_US/LC_MONETARY
-7fc37604c000-7fc37604d000 r--p 00000000 00:19 7661169                    /usr/lib/locale/en_AG/LC_MESSAGES/SYS_LC_MESSAGES
-7fc37604d000-7fc37604e000 r--p 00000000 00:19 7660948                    /usr/lib/locale/chr_US/LC_PAPER
-7fc37604e000-7fc37604f000 r--p 00000000 00:19 7660825                    /usr/lib/locale/bi_VU/LC_NAME
-7fc37604f000-7fc376050000 r--p 00000000 00:19 7661326                    /usr/lib/locale/en_US.utf8/LC_ADDRESS
-7fc376050000-7fc376051000 r--p 00000000 00:19 7660949                    /usr/lib/locale/chr_US/LC_TELEPHONE
-7fc376051000-7fc376052000 r--p 00000000 00:19 7660945                    /usr/lib/locale/chr_US/LC_MEASUREMENT
-7fc376052000-7fc376059000 r--s 00000000 00:19 7636497                    /usr/lib/x86_64-linux-gnu/gconv/gconv-modules.cache
-7fc376059000-7fc37605a000 r--p 00000000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
-7fc37605a000-7fc37607a000 r-xp 00001000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
-7fc37607a000-7fc376082000 r--p 00021000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
-7fc376082000-7fc376083000 r--p 00000000 00:19 7661327                    /usr/lib/locale/en_US.utf8/LC_IDENTIFICATION
-7fc376083000-7fc376084000 r--p 00029000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
-7fc376084000-7fc376085000 rw-p 0002a000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
-7fc376085000-7fc376086000 rw-p 00000000 00:00 0
-7ffffde56000-7ffffde79000 rw-p 00000000 00:00 0                          [stack]
-7ffffdebf000-7ffffdec3000 r--p 00000000 00:00 0                          [vvar]
-7ffffdec3000-7ffffdec5000 r-xp 00000000 00:00 0                          [vdso]
+55a33c431000-55a33c432000 r--p 00000000 00:19 8423013                    /home/marco/Documents/git/exOpSys/054showmem/Debug/054showmem
+55a33c432000-55a33c433000 r-xp 00001000 00:19 8423013                    /home/marco/Documents/git/exOpSys/054showmem/Debug/054showmem
+55a33c433000-55a33c434000 r--p 00002000 00:19 8423013                    /home/marco/Documents/git/exOpSys/054showmem/Debug/054showmem
+55a33c434000-55a33c435000 r--p 00002000 00:19 8423013                    /home/marco/Documents/git/exOpSys/054showmem/Debug/054showmem
+55a33c435000-55a33c436000 rw-p 00003000 00:19 8423013                    /home/marco/Documents/git/exOpSys/054showmem/Debug/054showmem
+55a33d713000-55a33d734000 rw-p 00000000 00:00 0                          [heap]
+7fabfa949000-7fabfa96e000 r--p 00000000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfa96e000-7fabfaab9000 r-xp 00025000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfaab9000-7fabfab03000 r--p 00170000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfab03000-7fabfab04000 ---p 001ba000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfab04000-7fabfab07000 r--p 001ba000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfab07000-7fabfab0a000 rw-p 001bd000 00:19 7636232                    /usr/lib/x86_64-linux-gnu/libc-2.31.so
+7fabfab0a000-7fabfab10000 rw-p 00000000 00:00 0
+7fabfab2c000-7fabfab2d000 r--p 00000000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
+7fabfab2d000-7fabfab4d000 r-xp 00001000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
+7fabfab4d000-7fabfab55000 r--p 00021000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
+7fabfab56000-7fabfab57000 r--p 00029000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
+7fabfab57000-7fabfab58000 rw-p 0002a000 00:19 7636228                    /usr/lib/x86_64-linux-gnu/ld-2.31.so
+7fabfab58000-7fabfab59000 rw-p 00000000 00:00 0
+7ffea399d000-7ffea39c0000 rw-p 00000000 00:00 0                          [stack]
+7ffea39c7000-7ffea39cb000 r--p 00000000 00:00 0                          [vvar]
+7ffea39cb000-7ffea39cd000 r-xp 00000000 00:00 0                          [vdso]
+
+indirizzo di main(): 0x55a33c4321c5
+indirizzo della variabile 'prova': 0x55a33c435078
+indirizzo della variabile 'param': 0x55a33c4350c0
+indirizzo della variabile 'program_filename' [stack]: 0x7ffea39bb620
+indirizzo di etext: 0x55a33c4323dd
+indirizzo di edata: 0x55a33c435082
+indirizzo di end: 0x55a33c4351c0
+
+etext  This is the first address past the end of the text segment (the program code).
+edata  This is the first address past the end of the initialized data segment.
+end    This is the first address past the end of the uninitialized data segment (also known as the BSS segment).
 
  */
 
