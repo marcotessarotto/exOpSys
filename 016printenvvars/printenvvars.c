61069f7b9cfc7d7099653dfc48fa8ad669d2bf19
marco <marcotts@gmail.com>
2021-05-10 23:15:38 +0200

    update

--- 016printenvvars/printenvvars.c
+++ 016printenvvars/printenvvars.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 extern char **environ;  /* Or define _GNU_SOURCE to get it from <unistd.h> */
 
@@ -11,8 +12,27 @@ int main(int argc, char *argv[])
 
     char **ep;
 
-    for (ep = environ; *ep != NULL; ep++)
+    for (ep = environ; *ep != NULL; ep++)  {
         puts(*ep);
+
+#ifdef SPLIT
+        char * str = strdup(*ep);
+        char s[] = "="; // caratteri separatori da utilizzare per la suddivisione
+        char *token;
+
+        // ottieni il primo token
+        token = strtok(str, s);
+
+        // "cammina" attraverso gli altri token
+        while (token != NULL ) {
+           printf( "+++%s\n", token );
+
+           token = strtok(NULL, s); // le chiamate successive a strtok
+        }
+#endif
+
+
+    }
 
     exit(EXIT_SUCCESS);
 }
