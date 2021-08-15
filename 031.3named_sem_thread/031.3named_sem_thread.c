4ad3a5ec1dabf313932f2dcc62d8dd4ccd0f72b0
marco <marcotts@gmail.com>
2021-08-15 14:25:28 +0200

    update

--- 031.3named_sem_thread/031.3named_sem_thread.c
+++ 031.3named_sem_thread/031.3named_sem_thread.c
@@ -20,9 +20,9 @@ char * semaphore_name = "/il_mio_semaforo_031.3";
 
 int shared_counter;
 
-#define N 1000000
+#define N 200000000
 
-#define THREADS 10
+#define THREADS 2
 
 
 void * do_operation(void * arg) {
@@ -58,6 +58,8 @@ void * do_operation(void * arg) {
 
 int main() {
 
+	struct timespec ts1, ts2;
+
 	sem_t * semaphore;
 
 	if (sem_unlink(semaphore_name) == -1) {
@@ -75,6 +77,8 @@ int main() {
 
 	pthread_t t[THREADS];
 
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts1);
+
 	for (int i = 0; i < THREADS; i++) {
 		if (pthread_create(&t[i], NULL, do_operation, NULL) != 0) {
 			perror("pthread_create");
@@ -88,8 +92,14 @@ int main() {
 		}
 	}
 
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts2);
+
 	printf("shared_counter = %d\n", shared_counter);
 	printf("risultato corretto? %s\n", shared_counter == N * THREADS ? "OK" : "NO");
+
+	unsigned long dt = ((ts2.tv_sec - ts1.tv_sec) * 1000000000L + ts2.tv_nsec) - ts1.tv_nsec;
+
+	printf("dt = %lu ns, %.3lf s\n", dt, dt / 1000000000.);
 
 
 	return 0;
